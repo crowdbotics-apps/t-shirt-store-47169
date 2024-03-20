ca3b4c68cada38a7726d6f3aed685254ce66babe
Crowdbotics <team@crowdbotics.com>
2024-03-20 21:52:36 +0000

    Initial commit

---
+++ backend/t_shirt_store_47169/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for t_shirt_store_47169 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "t_shirt_store_47169.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "t-shirt-store-47169",
+  url: "https://t-shirt-store-47169.botics.co",
+  api: "https://t-shirt-store-47169.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
